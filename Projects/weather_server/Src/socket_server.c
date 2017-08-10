ba4f276a20217ebd2a270013d6f4d5f588976a99
Gyuri20 <fodor.gyorgy.20@gmail.com>
2017-08-10 15:56:09 +0200

    refactoring socket server

--- Projects/weather_server/Src/socket_server.c
+++ Projects/weather_server/Src/socket_server.c
@@ -9,8 +9,6 @@
 #include "stm32746g_discovery_lcd.h"
 #include "GUI.h"
 #include "DIALOG.h"
-float received_weather_data[3];
-
 
 #define SERVER_QUEUE_SIZE 100
 #define SERVER_BUFF_LEN 100
@@ -20,6 +18,7 @@ float received_weather_data[3];
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+float received_weather_data[3];
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 void terminate_thread()
@@ -29,105 +28,95 @@ void terminate_thread()
 }
 // TODO:
 // Implement this function!
-
-
 void socket_server_thread(void const *argument)
 {
-		//BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+	LCD_UsrLog("Socket server - startup...\n");
+	LCD_UsrLog("Socket server - waiting for IP address...\n");
 
-		LCD_UsrLog("Socket server - startup...\n");
-		LCD_UsrLog("Socket server - waiting for IP address...\n");
+	// Wait for an IP address
+	while (!is_ip_ok())
+		osDelay(10);
+	LCD_UsrLog("Socket server - IP address is ok\n");
+	//GUI_DispString("Socket server - IP address is OK\n");
 
-		// Wait for an IP address
-		while (!is_ip_ok())
-			osDelay(10);
-		LCD_UsrLog("Socket server - IP address is ok\n");
-//		GUI_DispString("Socket server - IP address is OK\n");
+	// Create server socket
+	int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	if (server_socket < 0) {
+		LCD_ErrLog("Socket server - can't create socket\n");
+	//GUI_DispString("Socket server - cannot create socket\n");
+		terminate_thread();
+	}
+	LCD_UsrLog("Socket server - socket created\n");
+	//GUI_DispString("Socket server - socket created\n");
 
-		// Create server socket
-		int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-		if (server_socket < 0) {
-			LCD_ErrLog("Socket server - can't create socket\n");
-//			GUI_DispString("Socket server - cannot create socket\n");
-			terminate_thread();
-		}
-		LCD_UsrLog("Socket server - socket created\n");
-//		GUI_DispString("Socket server - socket created\n");
+	// Create address structure and bind the socket to it
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	if (bind(server_socket, (struct sockaddr*)&(server_addr), sizeof(server_addr)) < 0) {
+		LCD_ErrLog("Socket server - can't bind socket\n");
+	//GUI_DispString("Socket server - cannot bind socket\n");
+		terminate_thread();
+	}
+	LCD_UsrLog("Socket server - socket bind ok\n");
+	//GUI_DispString("Socket server - socket bind OK\n");
 
-		// Create address structure and bind the socket to it
-		struct sockaddr_in server_addr;
-		server_addr.sin_family = AF_INET;
-		server_addr.sin_port = htons(PORT);
-		server_addr.sin_addr.s_addr = INADDR_ANY;
-		if (bind(server_socket, (struct sockaddr*)&(server_addr), sizeof(server_addr)) < 0) {
-			LCD_ErrLog("Socket server - can't bind socket\n");
-//			GUI_DispString("Socket server - cannot bind socket\n");
-			terminate_thread();
-		}
-		LCD_UsrLog("Socket server - socket bind ok\n");
-//		GUI_DispString("Socket server - socket bind OK\n");
+	// Start listening
+	if (listen(server_socket, SERVER_QUEUE_SIZE) < 0) {
+		LCD_ErrLog("Socket server - can't listen\n");
+	//GUI_DispString("Socket server - cannot listen\n");
+		terminate_thread();
+	}
+	LCD_UsrLog("Socket server - listening...\n");
+	//GUI_DispString("Socket server - listening...\n");
 
-		// Start listening
-		if (listen(server_socket, SERVER_QUEUE_SIZE) < 0) {
-			LCD_ErrLog("Socket server - can't listen\n");
-//			GUI_DispString("Socket server - cannot listen\n");
-			terminate_thread();
-		}
-		LCD_UsrLog("Socket server - listening...\n");
-//		GUI_DispString("Socket server - listening...\n");
+	struct sockaddr_in client_addr;
+	socklen_t client_addr_len = sizeof(client_addr);
+	int client_socket;
 
+	while (1) {
+		// Accept incoming connections
+		client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
+		LCD_UsrLog("Socket server - connection accepted\n");
+		//GUI_DispString("Socket server connection accepted\n");
+		BSP_LCD_Clear(LCD_COLOR_GREEN);
+		// Check the client socket
+		if (client_socket < 0) {
+			LCD_ErrLog("Socket server - invalid client socket\n");
+			//GUI_DispString("Socket server - invalid client socket\n");
+		} else {
+			// Receive data
+			float received_bytes;
+			do {
+				received_bytes = recv(client_socket, received_weather_data, sizeof(received_weather_data), 0);
+				LCD_UsrLog("Temperature: %.1f C, Humidity: %.1f%%, Pressure: %.1f Pa,\n", received_weather_data[0], received_weather_data[1], received_weather_data[2]);
 
-		struct sockaddr_in client_addr;
-		socklen_t client_addr_len = sizeof(client_addr);
-		int client_socket;
-		//TS_StateTypeDef TS_State;
-		//TS_StateTypeDef *TS_State_ptr = &TS_State;
-		while (1) {
-			// Accept incoming connections
-			client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
-			LCD_UsrLog("Socket server - connection accepted\n");
-//			GUI_DispString("Socket server connection accepted\n");
-			BSP_LCD_Clear(LCD_COLOR_GREEN);
-			// Check the client socket
-			if (client_socket < 0) {
-				LCD_ErrLog("Socket server - invalid client socket\n");
-//				GUI_DispString("Socket server - invalid client socket\n");
-			} else {
-				// Define buffer for incoming message
-				//char buff[SERVER_BUFF_LEN];
-				float received_bytes;
-				//float buffer[3];
-				// Receive data
-				do {
-					
-					received_bytes = recv(client_socket, received_weather_data, sizeof(received_weather_data), 0);
-					LCD_UsrLog("Temperature: %.1f C, Humidity: %.1f%%, Pressure: %.1f Pa,\n", received_weather_data[0], received_weather_data[1], received_weather_data[2]);
+				GUI_GotoXY(167, 90);
+				GUI_SetFont(GUI_FONT_D64);	//display temperature
+				GUI_DispFloat(received_weather_data[0], 2);
 
-					GUI_GotoXY(167, 90);
-					GUI_SetFont(GUI_FONT_D64);	//display temperature
-					GUI_DispFloat(received_weather_data[0], 2);
+				GUI_GotoXY(318, 57);
+				GUI_SetFont(GUI_FONT_24_1);	//display humidity
+				GUI_DispFloat(received_weather_data[1], 4);
 
-					GUI_GotoXY(318, 57);
-					GUI_SetFont(GUI_FONT_24_1);	//display humidity
-					GUI_DispFloat(received_weather_data[1], 4);
+				GUI_GotoXY(312, 142);
+				GUI_DispFloat(received_weather_data[2]/ 100, 4);	//display pressure
+			} while (received_bytes > 0);
 
-					GUI_GotoXY(312, 142);
-					GUI_DispFloat(received_weather_data[2]/ 100, 4);	//display pressure
-				} while (received_bytes > 0);
+			// Close the socket
+			closesocket(client_socket);
+			LCD_UsrLog("Socket server - connection closed\n");
+			//GUI_DispString("Socket server - connection closed\n");
+		}//else
+	}//while(1)
 
-				// Close the socket
-				closesocket(client_socket);
-				LCD_UsrLog("Socket server - connection closed\n");
-//				GUI_DispString("Socket server - connection closed\n");
-			}
-		}
+	// Close socket
+	closesocket(server_socket);
 
-		// Close socket
-		closesocket(server_socket);
-
-		while (1) {
-			osDelay(10);
-		}
+	while (1) {
+		osDelay(10);
+	}
 }
 
 

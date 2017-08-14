63ba7b991fdbe1c49288207239c6fd161862502a
Gyuri20 <fodor.gyorgy.20@gmail.com>
2017-08-14 15:29:59 +0200

    add projector server test PC

---
+++ projector server/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <windows.h>
+
+// Create Server and Client Applications that send and receive messages.
+
+int main(int argc , char *argv[])
+{
+    WSADATA wsa;
+    SOCKET s , new_socket;
+    struct sockaddr_in server , client;
+    int c;
+    char *message;
+
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+    {
+        printf("Failed. Error Code : %d",WSAGetLastError());
+        return 1;
+    }
+
+    printf("Initialised.\n");
+
+    //Create a socket
+    if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d" , WSAGetLastError());
+    }
+
+    printf("Socket created.\n");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8003 );
+
+    //Bind
+    if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
+    {
+        printf("Bind failed with error code : %d" , WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+
+    puts("Bind done");
+
+    //Listen to incoming connections
+    listen(s , 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+
+    c = sizeof(struct sockaddr_in);
+    int buffer = 0;
+
+    while( (new_socket = accept(s , (struct sockaddr *)&client, &c)) != INVALID_SOCKET )
+    {
+        puts("Connection accepted");
+
+        //Reply to the client
+        int received_bytes = 0;
+
+        do {
+             received_bytes = recv(new_socket, &buffer, sizeof(buffer), 0);
+             printf("Buffer: %d ", buffer);
+             Sleep(500);
+        }while (received_bytes > 0);
+
+            //buffer[received_bytes] = '\0';
+            //send(new_socket, buffer, received_bytes, 0);
+       // } while (received_bytes > 0);
+
+        closesocket(new_socket);
+        //message = "Hello Client , I have received your connection. But I have to go now, bye\n";
+        //send(new_socket , buffer , strlen(buffer) , 0);
+    }
+
+    /*if (new_socket == INVALID_SOCKET)
+    {
+        printf("accept failed with error code : %d" , WSAGetLastError());
+        return 1;
+    }*/
+
+    closesocket(s);
+    WSACleanup();
+
+    return 0;
+}

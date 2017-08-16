cbb48fe078b430da1b33c592b7f54be16b72c8f3
chibbol <barbara.cegledi@gmail.com>
2017-08-16 11:48:16 +0200

    update How to

--- textformat.py
+++ textformat.py
@@ -1,3 +1,9 @@
+# How to use:
+# 1: Exchange required code between 'text' variable's double qoutations
+# 2: Open terminal, navigate to textformat.py's directory/folder
+# 3: Run it on terminal: 'python textformat.py'
+# 4: Et voila - you have the formatted file in 'textformat.txt'
+
 text = "put hex code here"
 def insert_newlines(text, every=80):
     lines = []

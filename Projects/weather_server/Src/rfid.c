f7b2a43381ec625dc76d078e24e5e7dc75a04bfa
Gyuri20 <fodor.gyorgy.20@gmail.com>
2017-08-23 10:10:40 +0200

    SPI HandleTypeDef modification

--- Projects/weather_server/Src/rfid.c
+++ Projects/weather_server/Src/rfid.c
@@ -5,34 +5,36 @@
 #include "cmsis_os.h"
 
 /* SPI handler declaration */
-SPI_HandleTypeDef spi_handle;
+SPI_HandleTypeDef hspi1;
 
 void spi_init(void)
 {
 	__HAL_RCC_SPI2_CLK_ENABLE();
 
-	spi_handle.Instance               = SPI2;
-	spi_handle.Init.Mode 			  = SPI_MODE_MASTER;
-	spi_handle.Init.Direction         = SPI_DIRECTION_2LINES;
-	spi_handle.Init.DataSize          = SPI_DATASIZE_8BIT; //?
-	spi_handle.Init.CLKPolarity       = SPI_POLARITY_LOW;
-	spi_handle.Init.CLKPhase          = SPI_PHASE_1EDGE;
-	spi_handle.Init.NSS               = SPI_NSS_HARD_OUTPUT; //or soft?
-	spi_handle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
-	spi_handle.Init.FirstBit          = SPI_FIRSTBIT_MSB;
-	spi_handle.Init.TIMode            = SPI_TIMODE_DISABLE;
-	spi_handle.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
-	spi_handle.Init.CRCPolynomial     = 7;
-	spi_handle.Init.CRCLength 		  = SPI_CRC_LENGTH_DATASIZE;
-	spi_handle.Init.NSSPMode 		  = SPI_NSS_PULSE_DISABLE; // or enable /SPI_NSS_SOFT
+	hspi1.Instance               = SPI2;
+	hspi1.Init.Mode 			 = SPI_MODE_MASTER;
+	hspi1.Init.Direction         = SPI_DIRECTION_2LINES;
+	hspi1.Init.DataSize          = SPI_DATASIZE_8BIT; //?
+	hspi1.Init.CLKPolarity       = SPI_POLARITY_LOW;
+	hspi1.Init.CLKPhase          = SPI_PHASE_1EDGE;
+	hspi1.Init.NSS               = SPI_NSS_SOFT; //or soft?
+	hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+	hspi1.Init.FirstBit          = SPI_FIRSTBIT_MSB;
+	hspi1.Init.TIMode            = SPI_TIMODE_DISABLE;
+	hspi1.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
+	hspi1.Init.CRCPolynomial     = 10;
+	hspi1.Init.CRCLength 		  = SPI_CRC_LENGTH_DATASIZE;
+	hspi1.Init.NSSPMode 		  = SPI_NSS_PULSE_DISABLE; // or enable /SPI_NSS_SOFT
 
-	HAL_SPI_Init(&spi_handle);
+	if (HAL_SPI_Init(&hspi1) != HAL_OK) {
+		while(1);
+	}
 }
 
-void transmit_spi(void)
-{
-	uint32_t buffer_trans;
-    HAL_GPIO_WritePin(GPIOI, GPIO_PIN_0, GPIO_PIN_RESET);
-	HAL_SPI_Receive(&spi_handle, &buffer_trans, 1, 1000);
-	HAL_GPIO_WritePin(GPIOI, GPIO_PIN_0, GPIO_PIN_SET);
-}
+//void transmit_spi(void)
+//{
+//	uint32_t buffer_trans;
+//    HAL_GPIO_WritePin(GPIOI, GPIO_PIN_0, GPIO_PIN_RESET);
+//	HAL_SPI_Receive(&hspi1, &buffer_trans, 1, 1000);
+//	HAL_GPIO_WritePin(GPIOI, GPIO_PIN_0, GPIO_PIN_SET);
+//}

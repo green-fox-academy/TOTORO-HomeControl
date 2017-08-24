b185877c70a6bd3f38fa54c94ac195bc0c84c4a9
Gyuri20 <fodor.gyorgy.20@gmail.com>
2017-08-24 12:00:07 +0200

    speed freq low ti high

--- Projects/weather_server/Src/stm32f7xx_hal_msp.c
+++ Projects/weather_server/Src/stm32f7xx_hal_msp.c
@@ -121,7 +121,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
 			GPIO_InitStruct.Pin       = GPIO_PIN_1;
 			GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
 			GPIO_InitStruct.Pull      = GPIO_NOPULL;
-			GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+			GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
 			GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
 			HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
 
@@ -129,7 +129,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
 			GPIO_InitStruct.Pin = GPIO_PIN_15;
 			GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
 		    GPIO_InitStruct.Pull = GPIO_NOPULL;
-		    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+		    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 		    GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
 			HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
@@ -137,7 +137,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
 			GPIO_InitStruct.Pin = GPIO_PIN_14;
 			GPIO_InitStruct.Mode      = GPIO_MODE_INPUT;
 			GPIO_InitStruct.Pull = GPIO_NOPULL;
-			GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+			GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 			GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
 			HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
@@ -145,7 +145,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
 		    GPIO_InitStruct.Pin = GPIO_PIN_0;
 		    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 		    GPIO_InitStruct.Pull = GPIO_NOPULL;
-		    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+		    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 //		    GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
 		    HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
 		}

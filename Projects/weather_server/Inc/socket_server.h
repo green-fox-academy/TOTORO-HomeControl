61c39fe01c6ea5729c79530fcd4c4da8ed9d3ded
chibbol <barbara.cegledi@gmail.com>
2017-08-24 19:26:27 +0200

    made struct global

--- Projects/weather_server/Inc/socket_server.h
+++ Projects/weather_server/Inc/socket_server.h
@@ -9,6 +9,25 @@
 /* Exported functions ------------------------------------------------------- */
 void socket_server_thread(void const *argument);
 void projector_server_thread(void const *argument);
+typedef struct  {
+   int tm_sec;         /* seconds,  range 0 to 59          */
+   int tm_min;         /* minutes, range 0 to 59           */
+   int tm_hour;        /* hours, range 0 to 23             */
+   int tm_mday;        /* day of the month, range 1 to 31  */
+   int tm_mon;         /* month, range 0 to 11             */
+   int tm_year;        /* The number of years since 1900   */
+   int tm_wday;        /* day of the week, range 0 to 6    */
+   int tm_yday;        /* day in the year, range 0 to 365  */
+   int tm_isdst;       /* daylight saving time             */
+}rtc_time;
+
+/*Structure for sending data to HQ */
+typedef struct {
+	float sensor_values[3];	// Storing Temperature, Humidity and Pressure values
+	rtc_time hq_time;
+}hq_data_t;
+
+hq_data_t received_data;
 
 
 //extern float received_weather_data[3];

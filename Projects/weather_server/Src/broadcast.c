f4898549ae56dc42b0dea7ab7830eafb68c1b007
Gyuri20 <fodor.gyorgy.20@gmail.com>
2017-09-05 15:32:06 +0200

    add broadcast.c

---
+++ Projects/weather_server/Src/broadcast.c
@@ -0,0 +1,12 @@
+/* Includes ------------------------------------------------------------------*/
+#include "socket_server.h"
+#include "lcd_log.h"
+#include "cmsis_os.h"
+#include "app_ethernet.h"
+#include "lwip/sockets.h"
+#include "stm32746g_discovery_ts.h"
+#include <string.h>
+#include "stm32746g_discovery_lcd.h"
+#include "GUI.h"
+#include "DIALOG.h"
+#include "broadcast.h"
